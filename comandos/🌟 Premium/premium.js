36fdecc94d647d3c766408ffd7ec5430856f1e99
fercho31 <87585299+fercho31@users.noreply.github.com>
2022-04-08 16:40:58 -0500

    Delete comandos directory

--- comandos/🌟 Premium/premium.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "premium",
-    desc: "Sirve para comprobar si eres Premium",
-    premium: true,
-    run: async (client, message, args, prefix) => {
-        message.reply(`<a:music:960705666354389104> Si ves este mensaje, significa que eres un usuario premium! 🌟`)
-    }
-}
